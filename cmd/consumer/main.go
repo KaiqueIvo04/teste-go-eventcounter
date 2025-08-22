5f0574f692f0b842cab729f604bafa259834460e
Kaique Ivo <kaiqueivo1011@gmail.com>
2025-08-21 23:39:47 -0300

    Chore: - Connect to RabbitMQ - Open RabbitMQ Channel - Consume Queue - Add environment variables

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
+	"github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	// Carrega variáveis de ambiente
+    err := godotenv.Load()
+    if err != nil {
+        log.Println("Não foi possível carregar o arquivo .env")
+    }
+
+	// Conectar com RabbitMq
+	connection, err := amqp091.Dial(os.Getenv("RABBITMQ_URI"))
+	if err != nil {
+		log.Fatalf("Erro ao tentar conectar no RabbitMq: %s", err)
+	}
+	defer connection.Close()
+
+	// Abrir um canal
+	channel, err := connection.Channel()
+	if err != nil {
+		log.Fatalf("Erro ao abrir canal: %s", err)
+	}
+	defer channel.Close()
+
+	// Declarar a fila a ser lida
+	queue, err := channel.QueueDeclare(os.Getenv("EXCHANGE"), true, false, false, false, nil)
+	if err != nil {
+		log.Fatalf("Erro ao declarar fila: %s", err)
+	}
+
+	// Consumir mensagens da fila
+	msgs, err := channel.Consume(queue.Name, "", true, false, false, false, nil)
+	if err != nil {
+		log.Fatalf("Erro ao registrar consumidor da fila: %s", err)
+	}
+
+	for msg := range msgs {
+		fmt.Printf("Mensagem: %s\n", msg.RoutingKey)
+	}
 }
